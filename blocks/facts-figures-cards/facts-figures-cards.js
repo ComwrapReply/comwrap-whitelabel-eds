c99b8d86bf0c9d577f579db3036befba246afbe0
ComwrapUkReply <s.sznajder@reply.com>
2025-09-25 13:57:15 +0100

    Update Facts and Figures Cards block JSON definitions: add 'title' component to 'facts-figures-card', ensure 'classes' field includes 'valueType' for better data handling, and streamline the structure for improved clarity and maintainability.

--- blocks/facts-figures-cards/facts-figures-cards.js
+++ blocks/facts-figures-cards/facts-figures-cards.js
@@ -1,20 +1,9 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
-  const rows = [...block.children];
-
   block.classList.add(`facts-figures-cards-${cols.length}-cols`);
-  block.classList.add(`facts-figures-cards-${rows.length}-rows`);
-
-  rows.forEach((row) => {
-    row.classList.add('facts-figures-cards-row');
-  });
-
-  cols.forEach((col) => {
-    col.classList.add('facts-figures-cards-col');
-  });
 
   // setup image columns
-  [...rows].forEach((row) => {
+  [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
